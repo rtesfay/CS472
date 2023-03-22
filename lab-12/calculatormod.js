081f13a6e58b747e26cd3f7b638793afd2085a7e
Rahel T Teklu <rahelteklu0@gmail.com>
2023-03-22 06:48:17 -0500

    lab12-comm

--- lab-12/calculatormod.js
+++ lab-12/calculatormod.js
@@ -1,3 +1,35 @@
+/**var result=0;
+exports.add = function (req,res,vals) {
+    result=parseInt(vals.first)+parseInt(vals.second)
+};
+exports.subtract = function (req,res,vals) {
+    result=parseInt(vals.first)-parseInt(vals.second)
+};
+exports.multiply = function (req,res,vals) {
+    result=parseInt(vals.first)*parseInt(vals.second)
+};
+exports.division = function (req,res,vals) {
+    result=(parseInt(vals.first))/(parseInt(vals.second))
+};
+exports.displays=function(req,res,vals){
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("<head><meta charset=\"utf-8\"/>"); 
+    res.write("<title>Calculator Web Site</title>"); 
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<p style=\"background-color:orange\" ; style=\"font:14pt bold\";>The result is: ");
+          res.write(String(result));
+    res.write("</p>");
+    res.write(
+        "<a href='https://rtesfay.github.io/CS472/lab-12/index.html/'>simplecalculator-page</a>"
+      );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+};*/
+
 var result=0;
 exports.add = function (req,res,vals) {
     result=parseInt(vals.first)+parseInt(vals.second)
